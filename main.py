983d573a567db06371b29192229ede7a3b0c29ef
Chakr3y <46889185+Chakr3y@users.noreply.github.com>
2023-06-25 19:43:26 +0700

    Add requirements.txt, simple program using PyMuPDF

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
+import tkinter
+from tkinter.filedialog import askopenfilename
 import fitz
 
 # Tesseract data on computer
 TESSDATA = "C:/Program Files/Tesseract-OCR/tessdata"
 
-# PDF document to scan
-file = "./Stellar_Engines.pdf"
+# language to OCR
+lang = "tha+eng"
 
-doc = fitz.open(file)
-p = doc.load_page(0)
+if __name__ == '__main__':
+	# prompt user to select a PDF to scan
+	tkinter.Tk().withdraw()
+	FILE = askopenfilename()
+	#FILE = "C:/Users/Chakrey/Documents/2023-06-23_185013.pdf"
 
-print(p.get_textpage_ocr(tessdata=TESSDATA).extractText())
 
-doc.close()
+	print("\nReading from " + FILE + "\n")
+	doc = fitz.open(FILE)
+	p = doc.load_page(0)
+	tp = p.get_textpage_ocr(full=False, language=lang, tessdata=TESSDATA, flags=8)
+
+	print(tp.extractText())
+
+	doc.close()
+	
