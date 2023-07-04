fc69274848fc92873c6dfac896a68111a7092421
Chakr3y <46889185+Chakr3y@users.noreply.github.com>
2023-07-04 15:29:22 +0700

    Change library to ocrmypdf

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sys, csv
 import tkinter
 from tkinter.filedialog import askopenfilename
-import fitz
+import ocrmypdf
 
 # Tesseract data on computer
 TESSDATA = "C:/Program Files/Tesseract-OCR/tessdata"
@@ -10,17 +11,11 @@ lang = "tha+eng"
 
 if __name__ == '__main__':
 	# prompt user to select a PDF to scan
-	tkinter.Tk().withdraw()
-	FILE = askopenfilename()
-	#FILE = "C:/Users/Chakrey/Documents/2023-06-23_185013.pdf"
-
+	"""tkinter.Tk().withdraw()
+	FILE = askopenfilename()"""
+	FILE = "C:/Users/Chakrey/Documents/2023-06-23_185013.pdf"
 
 	print("\nReading from " + FILE + "\n")
-	doc = fitz.open(FILE)
-	p = doc.load_page(0)
-	tp = p.get_textpage_ocr(full=False, language=lang, tessdata=TESSDATA, flags=8)
 
-	print(tp.extractText())
-
-	doc.close()
+	ocrmypdf.ocr(FILE, "output.pdf", language=lang)
 	
