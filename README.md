780a476f04c95523a800346f795a679483dd6605
Chakr3y <46889185+Chakr3y@users.noreply.github.com>
2023-07-21 16:58:51 +0700

    Completed basic Format seleciton menu.

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # ocr-research
 research project about OCR
 
+# instructions
+1. Run `python Main.py`.
+1. Select PDF files to scan. Multiple are acceptable.
+1. Select a Format or create a new one.
+	- Format creation (will rebound back to selection after)
+		1. Type a name for the Format.
+		1. Click on the PDF to make bounding boxes, where the program will later OCR the text.
+			- Due to limited editing functions, you will have to restart the process if you mess up.
+		1. Click next. The Format selection box will open again.
+1. The output file can be found in the output folder of the directory.
+
 # terminology
 - Format - a configuration that determines the areas to scan in each file, meaning that each file scanned needs to be in the same "format".
 
@@ -11,4 +22,4 @@ research project about OCR
 - Pillow
 - pytesseract
 
-**Note**: Make sure that the directory `C:/Program Files/Tesseract-OCR/tessdata` exists.
+**Note**: Make sure that the directory `C:/Program Files/Tesseract-OCR/tessdata` exists \(and that `tesseract` is registered as a CMD command?\).
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import configparser # config file
 import tkinter as tk
 from tkinter import ttk
 from tkinter.filedialog import askopenfilenames
+from tkinter.simpledialog import askstring
+from tkinter.messagebox import showerror
 import fitz
 from PIL import Image, ImageTk
 from FormatTools import *
@@ -31,46 +33,101 @@ if sys.platform == 'win32':
 	from ctypes import windll
 	windll.shcore.SetProcessDpiAwareness(1)
 
-# Top level window
-root = tk.Tk()
-root.title("OCR PDF")
-root.withdraw()
-
 
 ## FILE SELECTION
 FILES = askopenfilenames(filetypes=[('PDF Files','*.pdf')],initialfile=config['file']['lastopened'])
 if len(FILES) == 0:
 	print("No file selected, exiting.")
 	sys.exit()
-# save last opened file
+# save paths for last opened files
 config['file']['lastopened'] = str(FILES)[1:-1].replace(',','').replace("'",'"')
 with open(configpath, 'w') as cf: config.write(cf)
 print(f"\nReading from {[os.path.basename(x) for x in FILES]}\n")
+
 cur_doc = 0
-doc: fitz.Document = fitz.open(FILES[cur_doc])
-PAGES: int = doc.page_count # might be variable?
+doc: fitz.Document = None
+pages: int = None
 
 # Load new file onto 'doc' variable
 def load_doc(n: int):
-	global doc, cur_doc
-	doc.close()
+	global doc, cur_doc, pages
+	if doc is not None: doc.close()
 	doc = fitz.open(FILES[n])
 	cur_doc = n
+	pages = doc.page_count
+load_doc(cur_doc)
+
+# Top level window
+root = tk.Tk()
+root.title("OCR PDF")
+root.withdraw()
 
 # TODO: Format selection menu
+format_list: list[str] = None
+def format_select():
+	global format_list
+	# update list
+	format_list = [os.path.splitext(os.path.basename(x))[0] for x in os.listdir(os.path.join(DIR, "formats"))]
+
+	str_create = "Create a new Format..."
+	format_list.append(str_create)
+	
+	root.deiconify()
+	# frame container for widgets
+	frame = ttk.Frame(root, borderwidth=25)
+	frame.pack()
+
+	desc = ttk.Label(frame, text="Select a Format.")
+	desc.grid(row=0, column=0, sticky=tk.W)
+	# dropdown selection
+	select = ttk.Combobox(frame, exportselection=0, values=format_list)
+	select.grid(row=1, column=0, sticky=tk.W+tk.E)
+	# TODO: grey out button when none selected?
+	# buttons
+	def proceed():
+		selection = select.get()
+		if selection == "":
+			pass
+		elif selection == str_create:
+			root.withdraw()
+			frame.destroy()
+			create_format()
+			return
+		elif selection in format_list:
+			# TODO: implement previewing?
+			with open(os.path.join(DIR, "formats", selection+".pkl"), 'rb') as f:
+				format = pickle.load(f)
+				scan_docs(format)
+		else: pass
+		return		
+
+	btn_next = ttk.Button(frame, text="Next", command=proceed)
+	btn_next.grid(row=2, column=0, sticky=tk.E, pady=(20, 0))
 
 
 # Create a Format to specify where to scan for each page.
 def create_format():
-	new_format = Format()
+	f_name = askstring("OCR PDF", "Enter a name for the new Format.")
+	if f_name == "": # rebound back to format selection
+		format_select()
+		return
+	
+	while f_name in format_list: # duplicate name
+		showerror("Error", "This name has already been used by a Format.")
+		f_name = askstring("OCR PDF", "Enter a name for the new Format.", initialvalue=f_name)
+
+	root.deiconify()
+	format = Format(f_name)
+
+	# frame container for widgets
+	frame = ttk.Frame(root)
+	frame.pack(expand=1)
 
 	# configure row 1 (w/canvas) to stretch vertical
-	root.rowconfigure(1, weight=1)
-	# row 2 (bottom bar) to stretch horizontal
-	#root.rowconfigure(2,)
+	frame.rowconfigure(1, weight=1)
 
 	# instructions
-	instr = ttk.Label(root, text="Click to make bounding boxes that will be scanned.")
+	instr = ttk.Label(frame, text="Click to make bounding boxes that will be scanned.")
 	instr.grid(row=0, columnspan=2)
 
 	# Convert page to image
@@ -81,7 +138,7 @@ def create_format():
 		return Image.frombytes(mode, [pix.width, pix.height], pix.samples)
 
 	# pdf display
-	imgframe = ttk.LabelFrame(root, padding=5, labelanchor='n', text='%i/%i'%(1,PAGES))
+	imgframe = ttk.LabelFrame(frame, padding=5, labelanchor='n', text='%i/%i'%(1,pages))
 	imgframe.grid(row=1, column=1, sticky=tk.N+tk.S)
 
 	# load first page onto canvas
@@ -103,17 +160,15 @@ def create_format():
 			rect = fitz.Rect(p1, (e.x, e.y))
 			sa = ScanArea(pg, rect)
 			print(sa.__dict__)
-			new_format.append(sa)
-			
+			format.append(sa)
+			update_areanum()
 		else: p1 = (e.x, e.y)
 
-		print("%i, %i" % (e.x, e.y))
-		print(doc[pg].get_pixmap().pixel(e.x,e.y))
-
+		print("{e.x}, {e.y}")
+		#print(doc[pg].get_pixmap().pixel(e.x,e.y))
 		clicked = not clicked
 
 	canvas.bind('<Button-1>', canvas_click)
-
 
 	# Arrow page navigation
 	# have to load these outsize func else garbage collector will yeet them
@@ -121,8 +176,8 @@ def create_format():
 	arrowR = ImageTk.PhotoImage(Image.open(os.path.join(DIR,'assets/arrowR.png')).resize((32,64)))
 	ASSETCACHE.extend([arrowL, arrowR])
 	def arrow_nav_init():
-		arrowLLabel = tk.Label(root, image=arrowL)
-		arrowRLabel = tk.Label(root, image=arrowR)
+		arrowLLabel = tk.Label(frame, image=arrowL)
+		arrowRLabel = tk.Label(frame, image=arrowR)
 		arrowLLabel.grid(row=1, column=0, sticky=tk.E)
 		arrowRLabel.grid(row=1, column=2, sticky=tk.W)
 		def next_page(e): turn_page(1)
@@ -134,8 +189,8 @@ def create_format():
 	def turn_page(n: int):
 		nonlocal pg, tkimg
 		
-		pg = (pg + n) % PAGES
-		imgframe.config(text='%i/%i'%(pg+1,PAGES)) # update page label
+		pg = (pg + n) % pages
+		imgframe.config(text='%i/%i'%(pg+1,pages)) # update page label
 
 		tkimg = ImageTk.PhotoImage(page_to_img(pg))
 		canvas.itemconfig(imgid, image=tkimg)
@@ -144,8 +199,10 @@ def create_format():
 	arrow_nav_init()
 
 	# separate grid for bottom bar
-	bottom_bar = ttk.Frame(root, borderwidth=1)
-	bottom_bar.grid(row=2, column=0, columnspan=3, sticky=tk.W+tk.E)
+	b_line = ttk.Separator(frame)
+	b_line.grid(row=2, column=0, columnspan=3, sticky=tk.W+tk.E)
+	bottom_bar = ttk.Frame(frame, borderwidth=1)
+	bottom_bar.grid(row=3, column=0, columnspan=3, sticky=tk.W+tk.E)
 	bottom_bar.columnconfigure(3, weight=1)
 	# Document navigation
 	def docs_nav_init():
@@ -166,21 +223,33 @@ def create_format():
 		turn_page(0)
 
 	docs_nav_init()
+	# label for current document
 	doc_info = ttk.Label(bottom_bar, text="Doc #%i: %s"%(cur_doc+1, os.path.basename(FILES[cur_doc])))
 	doc_info.grid(row=0, column=2, sticky=tk.W)
+	
+	# display number of boxes made
+	area_num = ttk.Label(bottom_bar, text="Areas: 0")
+	area_num.grid(row=0, column=3)
+	def update_areanum():
+		area_num.config(text="Areas: %i"%len(format))
 
 	# 'Next' button
-	def next(): # TODO: handle when no areas have been selected
+	def proceed(): # TODO: handle when no areas have been selected
 		# TODO: continue to language selection
+		
+		# save format as a file
+		with open(os.path.join(DIR, "formats", format.name + ".pkl"), 'xb') as f:
+			pickle.dump(format, f)
 		root.withdraw()
-		root.quit()
-		scan_docs(new_format)
-	btn_next = ttk.Button(bottom_bar, text="Next", command=next)
-	btn_next.grid(row=0, column=3, sticky=tk.E)
+		frame.destroy()
+		format_select()
+	btn_next = ttk.Button(bottom_bar, text="Next", command=proceed)
+	btn_next.grid(row=0, column=4, sticky=tk.E)
 
 
 # Scan docs using selected format
 def scan_docs(format: Format):
+	root.quit()
 	ts = time.strftime('%b-%d-%Y_%H%M%S', time.localtime())
 	#doc[pg].get_pixmap(dpi=300, clip=rect).save()
 	with open(os.path.join(OUTPUT_DIR,ts+'.csv'), 'w', newline='') as f:
@@ -195,8 +264,7 @@ def scan_docs(format: Format):
 				rowdata.append(pytess.image_to_string(img, lang))
 			out.writerow(rowdata)
 
-create_format()
+format_select()
 
-root.deiconify()
 root.mainloop()
 doc.close()
